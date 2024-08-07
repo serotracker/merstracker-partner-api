4b441175482a7089ebf8b7a6f1bd8e6e940de64e
seank <seankenny6688@gmail.com>
2024-08-06 20:16:58 -0600

    Added CHANGELOG entry

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { generateMersPrimaryEstimatesRequestHandler } from './fao/mers-tracker-primary-estimates/index.js'
+import { generateMersPrimaryEstimatesRequestHandler } from './fao/merstracker-primary-estimates/index.js'
 import { MongoClient } from 'mongodb';
 
 interface GenerateRouterInput {
