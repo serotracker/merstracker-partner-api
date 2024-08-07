6b8519de2caaefb6bf36deeb22b35dce88117226
seank <seankenny6688@gmail.com>
2024-08-06 21:37:15 -0600

    fix the styles on the route name

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -23,6 +23,7 @@ const generateRouter = (input: GenerateRouterInput): GenerateRouterOutput => {
 
   router.get('/fao/merstracker-primary-estimates', mersPrimaryEstimatesRequestHandler);
   router.use('/api-docs', serve, setup(swaggerDocument, {
+    customCss: ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }",
     customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
   }));
 
