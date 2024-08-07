8902b80bf4b52af2ef82998ba7332d58f5af006c
seank <seankenny6688@gmail.com>
2024-08-07 12:56:01 -0600

    Changed the partitionKey to be a part of the query parameters rather than the body for estimates API

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -24,7 +24,7 @@ const generateRouter = (input: GenerateRouterInput): GenerateRouterOutput => {
   });
 
   router.get('/fao/merstracker-primary-estimate-partitions', mersPrimaryEstimatePartitionsRequestHandler);
-  router.post('/fao/merstracker-primary-estimates', mersPrimaryEstimatesRequestHandler);
+  router.get('/fao/merstracker-primary-estimates', mersPrimaryEstimatesRequestHandler);
   router.use('/api-docs', serve, setup(swaggerDocument, {
     customCss: ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }",
     customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
